8862e8c1f56b7ea5fe861936b6f04caf6e96421d
ndmerritt <nathandmerritt@gmail.com>
2026-04-08 18:14:09 -0400

    accumulus fixes

--- itis3135/projects/client/scripts/hover-video.js
+++ itis3135/projects/client/scripts/hover-video.js
@@ -1,12 +1,11 @@
-document.querySelectorAll('.card').forEach(card => {
+document.querySelectorAll('.card').forEach((card) => {
   const video = card.querySelector('video');
-
   if (!video) return;
 
   card.addEventListener('mouseenter', () => {
     video.muted = false;
     video.volume = 0.5;
-    video.play().catch(err => {
+    video.play().catch((err) => {
       console.log('Video could not play:', err);
     });
   });
