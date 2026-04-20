34f3e09af588315cbea0dec3c023a186389aa387
ndmerritt <nathandmerritt@gmail.com>
2026-04-20 17:55:10 -0400

    js component update

---
+++ itis3135/projects/client/scripts/sidebar.js
@@ -0,0 +1,18 @@
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+const portfolioLink = document.getElementById("portfolio-link");
+const closeBtn = document.getElementById("close-btn");
+
+function toggleMenu() {
+  document.body.classList.toggle("menu-open");
+}
+
+
+portfolioLink.addEventListener("click", function (e) {
+  e.preventDefault();
+  toggleMenu();
+});
+
+closeBtn.addEventListener("click", toggleMenu);
+
+overlay.addEventListener("click", toggleMenu);
