139f7a083f346f8c1ed3813b8ae0fb32e48be9e9
ndmerritt <nathandmerritt@gmail.com>
2026-04-26 17:42:41 -0400

    fix

---
+++ itis3135/projects/client/scripts/fadetext.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+  const elements = document.querySelectorAll(".fade-in");
+
+  elements.forEach((el, index) => {
+    setTimeout(() => {
+      el.classList.add("show");
+    }, index * 150);
+  });
+});
